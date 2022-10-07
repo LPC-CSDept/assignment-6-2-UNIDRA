4b951ede0a08a66f0a023bf44f124d1ae405cf40
UNIDRA <57227914+UNIDRA@users.noreply.github.com>
2022-10-06 19:05:52 -0700

    A62
    I was able to finish the function by using a for loop to act as an exponent would and returned the answer casted as a double. For the next function, I did almost the same but made the n positive in the for loop and then divided 1 by the result of the for loop which I casted into a double.

--- A62.hpp
+++ A62.hpp
@@ -8,10 +8,20 @@ using namespace std;
 double nthpowerp(int n)
 {
         // return n_th power number
+        int ans = 1;
+        for (int i = 0; i < n; i++) {
+                ans *= 2;
+        }
+        return (double)ans;
 }
 double nthpowern(int n)
 {
         // return n_th power number
+        int ans = 1;
+        for (int i = 0; i < n * -1; i++) {
+                ans *= 2;
+        }
+        return 1 / (double)ans;
 }
 void printout(double result)
 {
